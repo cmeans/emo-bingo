6b793b67407734a1fda7399bdf9626437ec5639d
Chris Means <cmeans@enova.com>
2020-10-04 14:15:58 -0500

    Added Storage and adjusted mobile breaks

--- src/components/NewEntry.vue
+++ src/components/NewEntry.vue
@@ -2,14 +2,14 @@
   <v-form ref="form" v-model="isFormValid">
     <v-container>
       <v-row>
-        <v-col class="col-sm-12 col-md-3">
+        <v-col class="col-md-12 col-lg-3">
           <v-text-field
             :rules="[v => !!v || 'Item is required']"
             v-model="name"
             label="Attempted Emotion"
           />
         </v-col>
-        <v-col class="col-sm-12 col-md-3">
+        <v-col class="col-md-12 col-lg-3">
           <v-file-input
             :rules="[v => !!v || 'Item is required']"
             v-model="image"
@@ -18,7 +18,7 @@
             capture="user"
           />
         </v-col>
-        <v-col class="col-sm-12 col-md-3">
+        <v-col class="col-md-12 col-lg-3">
           <v-btn
             :disabled="!isFormValid"
             v-on:click="saveEntry">
