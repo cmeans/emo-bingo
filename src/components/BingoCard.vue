6d62d7e734643b9ea944f8f0ed1eda69c51d415a
Chris Means <cmeans@enova.com>
2020-10-11 21:55:03 -0500

    Tweaking

--- src/components/BingoCard.vue
+++ src/components/BingoCard.vue
@@ -1,36 +1,65 @@
 <template>
-  <v-container>
-    <v-row>
-      <v-col cols="2" v-for="letter in 'B I N G O'.split(' ')" v-bind:key="letter">
-        <BingoCell :text="letter"></BingoCell>
-      </v-col>
-    </v-row>
-    <v-row v-for="row in [1,2,3,4,5]" v-bind:key="row">
-      <v-col cols="2" v-for="col in [1,2,3,4,5]" v-bind:key="col">
+  <v-card
+    outlined
+    elevation="2"
+    class="d-flex"
+  >
+    <v-container>
+      <v-row>
+        <v-col cols="2" v-for="letter in 'B I N G O'.split(' ')" v-bind:key="letter">
+          <BingoCell :text="letter"></BingoCell>
+        </v-col>
+      </v-row>
+      <div class="d-flex">
+        <v-flex flex-col v-for="row in [1,2,3,4,5]" :key="row">
+          <v-flex
+            auto
+            v-for="col in [1,2,3,4,5]"
+            :key="col"
+          >
+            <BingoCell>
+              <template v-slot:emotion>
+                <v-card-text>
+                  <div align="center">
+                    <div>
+                    {{ emotion().name }}
+                  </div>
+                  <v-img
+                    :src=emotion().icon
+                    :width="width"
+                  />
+                  </div>
+                </v-card-text>
+              </template>
+            </BingoCell>
+          </v-flex>
+        </v-flex>
+      </div>
+      <!-- <v-row v-for="row in [1,2,3,4,5]" v-bind:key="row">
+        <v-col cols="1" v-for="col in [1,2,3,4,5]" v-bind:key="col">
+        <v-flex auto v-for="col in [1,2,3,4,5]" :key="col">
           <BingoCell>
-            <template v-slot:emotion>
-              <v-card-text>
-                <div class="d-none d-md-flex justify-center">
-                  {{ emotion().name }}
-                </div>
-                <v-img
-                  contained
-                  :src=emotion().icon
-                  width="100%"
-                />
-              </v-card-text>
-            </template>
-          </BingoCell>
-      </v-col>
-    </v-row>
-  </v-container>
+              <template v-slot:emotion>
+                <v-card-text>
+                  <div class="d-none d-md-flex justify-center">
+                    {{ emotion().name }}
+                  </div>
+                  <v-img
+                    contained
+                    :src=emotion().icon
+                    width="100%"
+                  />
+                </v-card-text>
+              </template>
+            </BingoCell>
+        </v-flex>
+      </v-row> -->
+    </v-container>
+  </v-card>
 </template>
 
 <script>
   import BingoCell from './BingoCell';
-
-  // const EMOTIONS = 'HAPPY|SAD|ANGRY|CONFUSED|DISGUSTED|SURPRISED|CALM|FEAR'.split('|');
-  //const EMOTICONS = 'emoticon-happy|emoticon-sad|emoticon-angry|emoticon-confused|emoticon-poop|emoticon-tongue|emoticon-cool|emoticon-devil';
 
   export default {
     name: 'BingoCard',
@@ -44,6 +73,17 @@
     data: () => ({
 
     }),
+    computed: {
+      width() {
+        switch (this.$vuetify.breakpoint.name) {
+          case 'xs':
+          case 'sm':
+            return 40;
+        }
+
+        return 100;
+      }
+    },
     methods: {
       getRandomItem() {
         return this.items[Math.floor(Math.random() * this.items.length)];
--- src/components/BingoCell.vue
+++ src/components/BingoCell.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card class="d-flex align-center justify-center" height="100px" width="100px">
+  <v-card class="ma-2">
     <div v-resize-text="{ratio:2, minFontSize: '40px', maxFontSize: '200px', delay: 200}">
       {{ text }}
     </div>
